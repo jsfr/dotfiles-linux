ea16b72a33976115c019c8bfd9b38113ae1d9c74
Jens Fredskov <jensfredskov@protonmail.ch>
2016-09-26 15:36:52 +0200

    YOLO: touched...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ end
 set fish_greeting ""
 
 # define aliases
-alias aurau "sudo aur; sudo aura -Syu; sudo aura -Akua"
+alias aurau "sudo aura -Syu; sudo aura -Akua"
 alias aurac "sudo aura -Oj; and sudo paccache -r; and sudo paccache -ruk0; and sudo pacman-optimize"
 alias auras "sudo reflector --verbose -l 25 --sort rate --save /etc/pacman.d/mirrorlist"
 
