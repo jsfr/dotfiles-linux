84b30d3814037e9382d73429f22fb3bc7d9cb668
Jens Fredskov <jensfredskov@protonmail.ch>
2016-10-17 09:44:00 +0200

    YOLO: We should delete this crap before shipping.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,3 +19,5 @@ alias aurau "sudo aura -Syu; sudo aura -Akua"
 alias aurac "sudo aura -Oj; and sudo paccache -r; and sudo paccache -ruk0; and sudo pacman-optimize"
 alias auras "sudo reflector --verbose -l 25 --sort rate --save /etc/pacman.d/mirrorlist"
 alias ls "ls++"
+
+bass source ~/.profile
