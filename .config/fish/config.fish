b4a8e11937e02840a2959b4393c8c64d757ce1a1
Jens Fredskov <jensfredskov@protonmail.ch>
2016-10-23 21:20:31 +0200

    YOLO: more ignores

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,5 @@ set fish_greeting ""
 alias aurau "sudo aura -Syu; sudo aura -Akua"
 alias aurac "sudo aura -Oj; and sudo paccache -r; and sudo paccache -ruk0; and sudo pacman-optimize"
 alias auras "sudo reflector --verbose -l 25 --sort rate --save /etc/pacman.d/mirrorlist"
-alias ls "ls++"
 
 bass source ~/.profile
